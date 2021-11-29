ece957e6279df91c10126f60fa492c5e769ff320
PratheeshAcharath <pratheeshacharath0707@gmail.com>
2021-11-29 10:02:32 +0530

    new

--- validation.js
+++ validation.js
@@ -4,8 +4,8 @@ function validateform() {
     var uname = document.getElementById("uname").value;
     var psw = document.getElementById("psw").value;
     if (uname == "admin" && psw == "12345") {
-        alert ("Login successfully");
-        window.location = "";
+        // alert ("Login successfully");
+        window.location = "https://retail.ictkerala.org/mod/page/view.php?id=7427";
         return false;
     } else {
         alert("Username or Password is incorrect");
